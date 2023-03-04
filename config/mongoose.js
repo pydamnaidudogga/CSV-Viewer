3eae4df2211d32ae25c6d60194ced0e3540c9f19
PYDAMNAIDUDDOGGA <107361429+pydamnaidudogga@users.noreply.github.com>
2023-03-05 00:19:54 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/csvFiles',{useUnifiedTopology: true,useNewUrlParser: true});
+// const uri = 'mongodb://127.0.0.1/csvFiles';
+const uri = "mongodb+srv://pydamnaidu:D1OBXUD9rcUXnqwn@database.cjh6ckr.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(uri,{useUnifiedTopology: true,useNewUrlParser: true});
 mongoose.set('strictQuery', false)
 const db = mongoose.connection
 db.on('error',console.error.bind(console,'connection problem'))
